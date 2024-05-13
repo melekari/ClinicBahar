ef8b3dec38dafa574b15e3d7691e212982fb5bb2
Kemal Anıl Güvenç <82261358+Anilgvnc@users.noreply.github.com>
2024-05-13 21:36:34 +0300

    auth page added

--- clinic_bahar/web/js/router.js
+++ clinic_bahar/web/js/router.js
@@ -1,3 +1,6 @@
 function toHome() {
-    window.location.href = "../html/index.html"
+    window.location.href = "../html/index.html";
+}
+function toAccount() {
+    window.location.href = "../html/account.html";
 }
