7d3a3ba44f33e8f41969ed7bbee8d98aa6213c18
Melek Arı <melekari.ali@gmail.com>
2024-05-15 18:48:58 +0300

    Email Bug Fix

--- clinic_bahar/src/js/email.js
+++ clinic_bahar/src/js/email.js
@@ -1,30 +1,25 @@
-import {emailjs} from "https://cdn.emailjs.com/dist/email.min.js";
+
+function sendMail(){
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID= "service_dg5ee6d";
+  const templateID= "template_c9fsr4t";
   
-  emailjs.init("user_ReBNROvUztgbj_AoQ");
-
-  function sendEmail() {
-    // Prevent the form from submitting normally
-    event.preventDefault();
-
-    // Get form values
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const message = document.getElementById("message").value;
-
-    // Send email
-    emailjs.send("service_5rdlsqo", "template_olqjwdr", {
-      name: name,
-      email: email,
-      phone: phone,
-      message: message,
+  emailjs.send(serviceID,templateID,params)
+  .then(
+    res =>{
+      document.getElementById("name").value ="";
+      document.getElementById("email").value ="";
+      document.getElementById("phone").value ="";
+      document.getElementById("message").value ="";
+      console.log(res);
+      alert("your message sent successfully");
     })
-      .then(function(response) {
-        console.log("Email sent successfully!", response);
-        // Optionally, show a success message or redirect the user
-      }, function(error) {
-        console.error("Email could not be sent", error);
-        // Optionally, show an error message
-      });
-  }
+  .catch(err=>console.log(err));
+}
+
 
