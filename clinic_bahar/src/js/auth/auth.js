f084123c57f49a76656387784553924e12c5129d
Kemal Anıl Güvenç <82261358+Anilgvnc@users.noreply.github.com>
2024-05-15 19:53:16 +0300

    Update auth.js

--- clinic_bahar/src/js/auth/auth.js
+++ clinic_bahar/src/js/auth/auth.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,10 +23,13 @@ const auth = getAuth(app);
 
 const signUpBtn = document.getElementById('btnSignup');
 const signInBtn = document.getElementById('btnSignin');
+const signUpName = document.getElementById("signup-name");
 const signUpMail = document.getElementById("signup-mail");
 const signUpPassword = document.getElementById("signup-pswd");
 const signInMail = document.getElementById("signin-mail");
 const signInPassword = document.getElementById("signin-pswd");
+
+var name = signUpName.value;
 
 function signInWithEmailPassword() {
     var email = signInMail.value;
@@ -50,11 +54,8 @@ function signInWithEmailPassword() {
 }
 
 function signUpWithEmailPassword() {
-    var name = document.getElementById("signup-name");
     var email = signUpMail.value;
     var password = signUpPassword.value;
-
-    console.log(email, password);
 
     // [START auth_signup_password]
     createUserWithEmailAndPassword(auth, email, password)
@@ -62,6 +63,7 @@ function signUpWithEmailPassword() {
             // Signed in 
             var user = userCredential.user;
             alert('Account created successfully. Please SignIn to your account.');
+            writeUserData
             // ...
         })
         .catch((error) => {
@@ -70,6 +72,14 @@ function signUpWithEmailPassword() {
             console.log(errorCode, errorMessage);
         });
     // [END auth_signup_password]
+}
+
+
+function writeUserData(userId, name) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+        username: name
+    });
 }
 
 signUpBtn.addEventListener('click', signUpWithEmailPassword);
