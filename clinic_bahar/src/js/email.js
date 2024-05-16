5bf2f5bf22a803030affabd68960fb7e3f4f4690
Kemal Anıl Güvenç <82261358+Anilgvnc@users.noreply.github.com>
2024-05-16 19:37:39 +0300

    appointment page fix

--- clinic_bahar/src/js/email.js
+++ clinic_bahar/src/js/email.js
@@ -1,25 +1,32 @@
+function sendMail() {
+  const txtName = document.getElementById("name").value;
+  const txtEmail = document.getElementById("email").value;
+  const txtPhone = document.getElementById("phone").value;
+  const txtMessage = document.getElementById("message").value;
 
-function sendMail(){
+  if (txtName === '' || txtEmail === '' || txtPhone === '' || txtMessage === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+
   var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    message: document.getElementById("message").value,
+    name: txtName,
+    email: txtEmail,
+    phone: txtPhone,
+    message: txtMessage,
   };
-  const serviceID= "service_dg5ee6d";
-  const templateID= "template_c9fsr4t";
-  
-  emailjs.send(serviceID,templateID,params)
-  .then(
-    res =>{
-      document.getElementById("name").value ="";
-      document.getElementById("email").value ="";
-      document.getElementById("phone").value ="";
-      document.getElementById("message").value ="";
-      console.log(res);
-      alert("your message sent successfully");
-    })
-  .catch(err=>console.log(err));
+  const serviceID = "service_dg5ee6d";
+  const templateID = "template_c9fsr4t";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(
+      res => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+      })
+    .catch(err => console.log(err));
 }
-
-
